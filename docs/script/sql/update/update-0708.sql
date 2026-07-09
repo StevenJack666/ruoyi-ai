55dcba795799f6767cb820bb5896b3a9957bf2c1
zengxb <648669796@qq.com>
2026-07-09 11:42:23 +0800

    content:SQL提交

--- docs/script/sql/update/update-0708.sql
+++ docs/script/sql/update/update-0708.sql
@@ -6,6 +6,60 @@ INSERT INTO `ruoyi-ai-agent`.`sys_user` (`user_id`, `tenant_id`, `dept_id`, `use
 UPDATE `ruoyi-ai-agent`.`sys_config` SET `tenant_id` = '000000', `config_name` = '情报分类会话配置', `config_key` = 'intelClassify.session.config', `config_value` = '{\n    \"sessionId\":101,\n    \"sessionContent\":\"情报分类\",\n    \"sessionTitle\":\"情报分类\"\n}', `config_type` = 'N', `create_dept` = 103, `create_by` = 1, `create_time` = '2026-07-07 16:57:42', `update_by` = 1, `update_time` = '2026-07-08 14:48:20', `remark` = NULL WHERE `config_id` = 2074417562781937666;
 UPDATE `ruoyi-ai-agent`.`sys_config` SET `tenant_id` = '000000', `config_name` = '风险研判会话配置', `config_key` = 'riskJudge.session.config', `config_value` = '{\n    \"sessionId\":100,\n    \"sessionContent\":\"风险研判\",\n    \"sessionTitle\":\"风险研判\"\n}', `config_type` = 'N', `create_dept` = 103, `create_by` = 1, `create_time` = '2026-07-07 16:58:04', `update_by` = 1, `update_time` = '2026-07-08 14:47:59', `remark` = NULL WHERE `config_id` = 2074417653013999617;
 
+
+DROP TABLE IF EXISTS `session_message_file_rel`;
+CREATE TABLE `session_message_file_rel`  (
+`id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+`message_id` bigint(20) NULL DEFAULT NULL COMMENT '对话消息ID',
+`session_id` bigint(20) NULL DEFAULT NULL COMMENT '消息ID',
+`oss_file_id` bigint(20) NULL DEFAULT NULL COMMENT '文件ID',
+`create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+`update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+`create_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建者',
+`update_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '更新者',
+`remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
+`is_deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除（0：未删除，1：已删除）',
+PRIMARY KEY (`id`) USING BTREE,
+INDEX `idx_message_id`(`message_id` ASC) USING BTREE,
+INDEX `idx_oss_file_id`(`oss_file_id` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2070449697850568707 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '会话消息文件关联表' ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `resource_info`;
+CREATE TABLE `resource_info`  (
+`id` bigint(20) NOT NULL AUTO_INCREMENT,
+`resource_uri` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`status` smallint(6) NULL DEFAULT NULL COMMENT '是否拦截：0（拦截），1（放行）',
+PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `user_authen_info`;
+CREATE TABLE `user_authen_info`  (
+ `id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `app_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+ `security_key` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+ `security_type` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'app_id对应的验签方式(01-原方式，请求参数都参与,02-只针对appid进行参与签名)',
+ `organization` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '所属机构',
+ `un_security_key` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+ PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `user_permission_ip`;
+CREATE TABLE `user_permission_ip`  (
+`id` bigint(20) NOT NULL AUTO_INCREMENT,
+`app_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`ip` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `user_permission_resource`;
+CREATE TABLE `user_permission_resource`  (
+`id` bigint(20) NOT NULL AUTO_INCREMENT,
+`app_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+`resource_uri` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
 DROP TABLE IF EXISTS `rds_data_flow_config`;
 CREATE TABLE `rds_data_flow_config`  (
  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '流控配置ID',
